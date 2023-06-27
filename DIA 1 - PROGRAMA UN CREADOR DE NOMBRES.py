9f012c87ce5a91ebe31079953f91e1de58091905
Rodolfo <xfazziox8@gmail.com>
2023-06-27 19:18:39 -0400

    primer commit

---
+++ DIA 1 - PROGRAMA UN CREADOR DE NOMBRES.py
@@ -0,0 +1,17 @@
+'Clase de print'
+print('Me encanta estudiar Python')
+print('Estudiar con "Python Total" es super divertido')
+print(550 + 5)
+
+'Clase de string'
+print('Linia 1\nLinea 2\nLinea 3')
+print('A\tB\tC\tD\tE\tF\tG\tH')
+print('Barra / \nBarra \\')
+
+'Clase de input'
+print(('Tu estas estudiando ') + input('¿Qué estás estudiando?\n '))
+print(('Tu vives en ') + input('¿En qué país vives?\n ' ))
+print(('Tu nombre es ') + input('Escribe tu Nombre:\n ') + ' ' + input('Escribe tu Apellido:\n '))
+
+'Proyecto'
+print('La Cerveza\nse llama "' + input('¿En donde vives? ') + ' ' + input('¿Que planeta te gusta? ') + '"\nFelicitaciones!')
---
+++ DÍA 2 - PROGRAMA UN CALCULADOR DE COMISIONES.py
@@ -0,0 +1,85 @@
+'Clase de Variable'
+nombre = 'Tony Soprano'
+edad = 51
+print(nombre, edad)
+
+nombre1 = 'Julia '
+apellido = 'Roberts'
+nombrecompleto = (nombre1 + apellido)
+print('Tu nombre es ' + nombrecompleto)
+
+curso = 'Python'
+print('Estas tomando un curso para ello ' + curso)
+
+'Clase de Nombre de Variables'
+'1_ Legible: nombre de la variable es relevante según su contenido'
+'2_ Unidad: no existen espacios (puedes incorporar guiones bajos)'
+'3_ Hispanismos: omitir signos específicos del idioma español, como tildes o la letra ñ'
+'4_ Números: los nombres de las variables no deben empezar por números (aunque pueden contenerlos al final)'
+'5_ Signos/símbolos: no se deben incluir : " , < > / ? | \ ( ) ! @ # $ % ^ & * ~ - +'
+'6_ Palabras clave: no utilizamos palabras reservadas por Python'
+
+'Clase de Integers & Floats'
+num_entero = 10
+print(num_entero)
+print(type(num_entero))
+
+num_decimal = 5.9
+print(num_decimal)
+print(type(num_decimal))
+
+num1 = 7.5
+num2 = 2.3
+print(num1 + num2)
+print(type(num1 + num2))
+
+'Clase de Conversiones'
+num1 = 7.5
+print(int(num1))
+print(type(int(num1)))
+
+num2 = 10
+print(float(num2))
+print(type(float(num2)))
+
+print(num1+num2)
+print(type(num1+num2))
+
+'Clase de Formatear Cadenas'
+nombre_asociado = 'Jesse Pinkman'
+numero_asociado = 399058
+print(f'Estimado/a {nombre_asociado}, su número de asociado es: {numero_asociado}')
+print('Estimado/a {}, su número de asociado es: {}'.format(nombre_asociado, numero_asociado))
+
+puntos_nuevos = 350
+puntos_totales = 1225
+print(f'Has ganado {puntos_nuevos} puntos! En total, acumulas {puntos_totales} puntos')
+print('Has ganado {} puntos! En total, acumulas {} puntos'.format(puntos_nuevos, puntos_totales))
+
+puntos_nuevos1 = 350
+puntos_anteriores = 875
+print(f'Has ganado {puntos_nuevos1} puntos! En total, acumulas {puntos_nuevos + puntos_anteriores} puntos')
+
+'Clase de Operadores Matematicos'
+a = 874
+b = 27
+print(f'{a} divido al piso de {b} es igual {a//b}')
+
+c = 456
+d = 33
+print(f'{c} modulo de {d} es igual {c%d}')
+
+e = 783
+print(f'la raiz cuadra de {e} es igual {e**0.5}')
+
+'Clase de Redondeo'
+num1 = 10/3
+print(round(num1, 2))
+
+num2 = 10.676767
+print(round(num2))
+
+num3 = 5
+print(round(5**0.5, 4))
+
+'Proyecto'
